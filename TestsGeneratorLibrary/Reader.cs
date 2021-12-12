3161565e0e46685925da3bf092ca4cbf28e1a6c3
S1mpleKnight <become.adult77@gmail.com>
2021-12-12 15:31:55 +0300

    new: create Reader & Writer

---
+++ TestsGeneratorLibrary/Reader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+     public class Reader
+     {
+          public async Task<string> ReadAsync(string path)
+          {
+               using (StreamReader streamReader = new StreamReader(path))
+               {
+                    return await streamReader.ReadToEndAsync();
+               }
+          }
+     }
+}
---
+++ TestsGeneratorLibrary/Writer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using TestsGeneratorLibrary.Structure;
+
+namespace TestsGeneratorLibrary
+{
+     public class Writer
+     {
+          private readonly string pathToOutputDirectory;
+
+          public Writer(string pathToOutputDirectory)
+          {
+               this.pathToOutputDirectory = pathToOutputDirectory;
+               if (!Directory.Exists(pathToOutputDirectory))
+               {
+                    Directory.CreateDirectory(pathToOutputDirectory);
+               }
+          }
+
+          public async Task WriteAsync(TestClassStructure generatedCode)
+          {
+               string filePath = String.Format("{0}\\{1}",  pathToOutputDirectory, generatedCode.TestClassName);
+               using (StreamWriter streamWriter = new StreamWriter(filePath))
+               {
+                    await streamWriter.WriteAsync(generatedCode.TestClassData);
+               }
+          }
+     }
+}
