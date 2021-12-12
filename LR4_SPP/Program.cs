df46c834a73e1e81c6f0fa11f41aa309848af21f
S1mpleKnight <become.adult77@gmail.com>
2021-12-12 15:38:28 +0300

    new: create execution case

---
+++ LR4_SPP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsGeneratorLibrary;
+
+namespace LR4_SPP
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               string outputPath = "D:\\Tests";
+               string path = "D:\\Code\\Stealed\\Spp\\LR4_SPP-master\\TestsGeneratorLibrary\\Writer.cs";
+
+               List<string> pathes = new List<string>();
+               pathes.Add(path);
+
+               Config config = new Config(3, 3, 3);
+               TestsGenerator generator = new TestsGenerator(config);
+
+               generator.Generate(new Reader(), new Writer(outputPath), pathes).Wait();
+          }
+     }
+}
