d75f180270c198ddf46cd19794929bb9d71268b8
S1mpleKnight <become.adult77@gmail.com>
2021-12-12 15:31:11 +0300

    new: create TestGEnerator & Template

---
+++ TestsGeneratorLibrary/TestClassTemplate.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using TestsGeneratorLibrary.Structure;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace TestsGeneratorLibrary
+{
+     internal class TestClassTemplate
+     {
+          private readonly TreeStructure treeStructure;
+
+          public TestClassTemplate(TreeStructure treeStructure)
+          {
+               this.treeStructure = treeStructure;
+          }
+
+          public List<TestClassStructure> GetTestTemplates()
+          {
+               List<TestClassStructure> testTemplates = new List<TestClassStructure>();
+
+               foreach (ClassInfo classInfo in treeStructure.Classes)
+               {
+                    CompilationUnitSyntax compilationUnit = CompilationUnit()
+                         //используемые директивы
+                         .WithUsings(GetUsingDirectives(classInfo))
+                         //namespace
+                         .WithMembers(SingletonList<MemberDeclarationSyntax>(GetNamespaceDeclaration(classInfo)
+                         //имена тестовых классов
+                         .WithMembers(SingletonList<MemberDeclarationSyntax>(ClassDeclaration(classInfo.ClassName + "Tests")
+                              .WithAttributeLists(
+                                   SingletonList<AttributeListSyntax>(
+                                   AttributeList(
+                                        SingletonSeparatedList<AttributeSyntax>(
+                                             Attribute(
+                                                  IdentifierName("TestClass"))))))
+                              //модификаторы доступа
+                              .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
+                              //публичные методы
+                              .WithMembers(GetMembersDeclarations(classInfo))))
+                        )
+                     );
+
+
+                    string fileName = String.Format("{0}Test.cs.test", classInfo.ClassName);
+                    string fileData = compilationUnit.NormalizeWhitespace().ToFullString();
+
+                    testTemplates.Add(new TestClassStructure(fileName, fileData));
+               }
+
+               return testTemplates;
+          }
+
+          private NamespaceDeclarationSyntax GetNamespaceDeclaration(ClassInfo classInfo)
+          {
+               NamespaceDeclarationSyntax namespaceDeclaration = 
+                    NamespaceDeclaration(QualifiedName(
+                         IdentifierName(classInfo.NamespaceName), IdentifierName("Tests")));
+
+               return namespaceDeclaration;
+          }
+
+          private SyntaxList<MemberDeclarationSyntax> GetMembersDeclarations(ClassInfo classInfo)
+          {
+               List<MemberDeclarationSyntax> methods = new List<MemberDeclarationSyntax>();
+
+               methods.Add(GetMethodDeclaration("Initialize", "TestInitialize", new List<StatementSyntax>()));
+               foreach (MethodInfo method in classInfo.Methods)
+               {
+                    List<StatementSyntax> bodyMembers = new List<StatementSyntax>();
+                    bodyMembers.Add(
+                    ExpressionStatement(
+                         InvocationExpression(
+                              GetAssertFail())
+                                   .WithArgumentList(GetMemberArgs())));
+                    methods.Add(GetMethodDeclaration(method.Name + "Test", "TestMethod", bodyMembers));
+               }
+               return new SyntaxList<MemberDeclarationSyntax>(methods);
+          }       
+
+          private MemberDeclarationSyntax GetMethodDeclaration(String methodName, String atribute, List<StatementSyntax> bodyMembers )
+          {              
+               MethodDeclarationSyntax methodDeclaration = MethodDeclaration(
+                    PredefinedType(
+                         Token(SyntaxKind.VoidKeyword)),
+                              Identifier(methodName))
+                                   .WithAttributeLists(
+                                        SingletonList<AttributeListSyntax>(
+                              AttributeList(
+                                   SingletonSeparatedList<AttributeSyntax>(
+                                        Attribute(
+                                             IdentifierName(atribute))))))
+                              .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
+                              .WithBody(Block(bodyMembers));
+
+               return methodDeclaration;
+          }
+
+          private ArgumentListSyntax GetMemberArgs()
+          {
+               ArgumentListSyntax args = ArgumentList(
+                   SingletonSeparatedList(
+                       Argument(
+                           LiteralExpression(
+                               SyntaxKind.StringLiteralExpression,
+                               Literal("Generated")))));
+
+               return args;
+          }
+
+          private MemberAccessExpressionSyntax GetAssertFail()
+          {
+               MemberAccessExpressionSyntax assertFail = MemberAccessExpression(
+                   SyntaxKind.SimpleMemberAccessExpression,
+                   IdentifierName("Assert"),
+                   IdentifierName("Fail"));
+
+               return assertFail;
+          }
+
+          private SyntaxList<UsingDirectiveSyntax> GetUsingDirectives(ClassInfo classInfo)
+          {
+               List<UsingDirectiveSyntax> usingDirectives = new List<UsingDirectiveSyntax>();
+
+               usingDirectives.Add(UsingDirective(IdentifierName("System")));
+               usingDirectives.Add(UsingDirective(IdentifierName("Microsoft.VisualStudio.TestTools.UnitTesting")));
+               usingDirectives.Add(UsingDirective(IdentifierName(classInfo.NamespaceName)));
+
+               return new SyntaxList<UsingDirectiveSyntax>(usingDirectives);
+          }
+     }
+}
---
+++ TestsGeneratorLibrary/TestsGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using TestsGeneratorLibrary.Structure;
+
+namespace TestsGeneratorLibrary
+{
+     public class TestsGenerator
+     {
+          private readonly Config config;
+
+          public TestsGenerator(Config config)
+          {
+               this.config = config;
+          }
+
+          public async Task Generate(Reader reader, Writer writer, List<string> source)
+          {
+               //для уведомления о завершении считывания
+               DataflowLinkOptions linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+               //количество одновременно считываемых файлов
+               ExecutionDataflowBlockOptions maxReading = new ExecutionDataflowBlockOptions
+               {
+                    MaxDegreeOfParallelism = config.ReadingTasksCount
+               };
+
+               //количество одновременно обрабатываемых файлов
+               ExecutionDataflowBlockOptions maxProcessing = new ExecutionDataflowBlockOptions {
+                    MaxDegreeOfParallelism = config.ProcessingTasksCount
+               };
+
+               //количество одновременно записываемых файлов
+               ExecutionDataflowBlockOptions maxWriting = new ExecutionDataflowBlockOptions
+               {
+                    MaxDegreeOfParallelism = config.WritingTasksCount
+               };
+
+               TransformBlock<string, string> readingBlock =
+                    new TransformBlock<string, string>(fileName => reader.ReadAsync(fileName), maxReading);
+
+               TransformBlock<string, TestClassStructure> producingBlock =
+                    new TransformBlock<string, TestClassStructure>(sourceCode => Produce(sourceCode), maxProcessing);
+
+               ActionBlock<TestClassStructure> writingBlock = 
+                    new ActionBlock<TestClassStructure>(generatedClass => writer.WriteAsync(generatedClass), maxWriting);
+
+               readingBlock.LinkTo(producingBlock, linkOptions);
+               producingBlock.LinkTo(writingBlock, linkOptions);
+
+               foreach (string path in source)
+               {
+                    await readingBlock.SendAsync(path);
+               }
+
+               //сообщение о завершении считываемых файлов
+               readingBlock.Complete();
+
+               await writingBlock.Completion;
+          }
+
+          private TestClassStructure Produce(string sourceCode)
+          {
+               TreeBuilder treeBuilder = new TreeBuilder(sourceCode);
+               TreeStructure treeStructure = treeBuilder.GetTreeStructure();
+
+               TestClassTemplate testTemplatesGenerator = new TestClassTemplate(treeStructure);
+               List<TestClassStructure> testTemplates = testTemplatesGenerator.GetTestTemplates().ToList();
+
+               return new TestClassStructure(testTemplates.First().TestClassName, testTemplates.First().TestClassData); 
+          }
+     }
+}
