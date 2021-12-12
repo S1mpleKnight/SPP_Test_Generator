1d1ffc76f0e615d7dc2b059dc21d59df2b66dad7
S1mpleKnight <become.adult77@gmail.com>
2021-12-12 15:33:52 +0300

    new: add configs

---
+++ TestsGeneratorLibrary/Config.cs
@@ -0,0 +1,34 @@
+﻿namespace TestsGeneratorLibrary
+{
+     public class Config
+     {
+          private int readingTasksCount;
+          private int processingTasksCount;
+          private int writingTasksCount;
+
+          public int ReadingTasksCount
+          {
+               get { return readingTasksCount; }
+               set { readingTasksCount = value; }
+          }
+
+          public int ProcessingTasksCount
+          {
+               get { return processingTasksCount; }
+               set { processingTasksCount = value; }
+          }
+
+          public int WritingTasksCount
+          {
+               get { return writingTasksCount; }
+               set { writingTasksCount = value; }
+          }
+
+          public Config(int readingTasksCount, int processingTasksCount, int writingTasksCount)
+          {
+               this.readingTasksCount = readingTasksCount;
+               this.processingTasksCount = processingTasksCount;
+               this.writingTasksCount = writingTasksCount;
+          }
+     }
+}
